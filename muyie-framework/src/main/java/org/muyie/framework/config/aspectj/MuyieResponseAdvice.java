c06d37841b4552b8de0f24dc2ecf275a321b747b
qizhaohong <qizhaohong_sh@qq.com>
2019-10-13 21:56:52 +0800

    Initial commit

---
+++ muyie-framework/src/main/java/org/muyie/framework/config/aspectj/MuyieResponseAdvice.java
@@ -0,0 +1,50 @@
+package org.muyie.framework.config.aspectj;
+
+import org.muyie.framework.config.MuyieProperties;
+import org.muyie.framework.http.Result;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.zalando.problem.Problem;
+
+@Configuration
+@ConditionalOnWebApplication
+@AutoConfigureAfter(MuyieProperties.class)
+@RestControllerAdvice
+public class MuyieResponseAdvice implements ResponseBodyAdvice<Object> {
+
+  @Override
+  public boolean supports(MethodParameter returnType,
+      Class<? extends HttpMessageConverter<?>> converterType) {
+    return true;
+  }
+
+  @Override
+  public Object beforeBodyWrite(Object body, MethodParameter returnType,
+      MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+      ServerHttpRequest request, ServerHttpResponse response) {
+
+    // Handle Problem Exception
+    if (body instanceof Problem) {
+      Problem problem = (Problem) body;
+      return Result.of(String.valueOf(problem.getStatus().getStatusCode()), problem.getTitle());
+    }
+
+    // Handle REST Response headers
+    if (body instanceof Result) {
+      Result result = (Result) body;
+      response.getHeaders().addAll(result.getHeaders());
+      return result;
+    }
+
+    return body;
+  }
+
+}

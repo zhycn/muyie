81a02be3f224196b410cda1694c18e142cc77ae8
qizhaohong <qizhaohong_sh@qq.com>
2019-10-13 12:22:45 +0800

    Initial commit

---
+++ muyie-framework/src/main/java/org/muyie/framework/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.muyie.framework.exception;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.muyie.framework.http.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.google.common.base.Throwables;
+
+@Configuration
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class GlobalExceptionHandler {
+
+  private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  @ExceptionHandler(MuyieException.class)
+  public Result handleException(MuyieException e, HttpServletResponse response) {
+    int sc = e.getStatus().value();
+    log.error(sc != 200 ? Throwables.getStackTraceAsString(e) : e.getMessage());
+    return Result.of(e.getResultCode());
+  }
+
+}
---
+++ muyie-framework/src/main/java/org/muyie/framework/exception/MuyieException.java
@@ -0,0 +1,71 @@
+package org.muyie.framework.exception;
+
+import org.muyie.framework.http.ResultCode;
+import org.springframework.http.HttpStatus;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * Runtime exception provider for MuYie
+ */
+public class MuyieException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+  private final ResultCode resultCode;
+  private final HttpStatus status;
+
+  public MuyieException(ResultCode resultCode) {
+    this(resultCode, HttpStatus.OK);
+  }
+
+  public MuyieException(ResultCode resultCode, String message) {
+    this(resultCode, HttpStatus.OK, message);
+  }
+
+  public MuyieException(ResultCode resultCode, String message, Throwable cause) {
+    this(resultCode, HttpStatus.OK, message, cause);
+  }
+
+  public MuyieException(ResultCode resultCode, Throwable cause) {
+    this(resultCode, HttpStatus.OK, cause);
+  }
+
+  public MuyieException(ResultCode resultCode, HttpStatus status) {
+    this.resultCode = resultCode;
+    this.status = status;
+  }
+
+  public MuyieException(ResultCode resultCode, HttpStatus status, String message) {
+    super(message);
+    this.resultCode = resultCode;
+    this.status = status;
+  }
+
+  public MuyieException(ResultCode resultCode, HttpStatus status, String message, Throwable cause) {
+    super(message, cause);
+    this.resultCode = resultCode;
+    this.status = status;
+  }
+
+  public MuyieException(ResultCode resultCode, HttpStatus status, Throwable cause) {
+    super(cause);
+    this.resultCode = resultCode;
+    this.status = status;
+  }
+
+  public ResultCode getResultCode() {
+    return resultCode;
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+
+  @Override
+  public String getMessage() {
+    return StrUtil.format("ResultCode={}#{}, Message={}", resultCode.getCode(), resultCode.getMsg(),
+        super.getMessage());
+  }
+
+}

343dd80956dddef622c3670f662fb36bd560944e
qizhaohong <qizhaohong_sh@qq.com>
2020-06-04 14:11:12 +0800

    update

--- muyie-framework/src/main/java/org/muyie/framework/util/AssertUtil.java
+++ muyie-framework/src/main/java/org/muyie/framework/util/AssertUtil.java
@@ -21,8 +21,6 @@ public final class AssertUtil {
    *
    * @param object object to validate
    * @param groups the group or list of groups targeted for validation (defaults to {@link Default})
-   * @param <T> the type of the object to validate
-   * @return constraint violations or an empty set if none
    * @throws IllegalArgumentException if object is {@code null} or if {@code null} is passed to the
    *         varargs groups
    * @throws ValidationException if a non recoverable error happens during the validation process

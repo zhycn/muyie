31f62d062b38ff87a6762ea7955bb9970c465149
qizhaohong <qizhaohong_sh@qq.com>
2022-08-11 16:51:07 +0800

    Initial commit

--- muyie-starter-apollo/src/main/java/com/muyie/apollo/MuyieApolloAutoConfiguration.java
+++ muyie-starter-apollo/src/main/java/com/muyie/apollo/MuyieApolloAutoConfiguration.java
@@ -1,10 +1,12 @@
 package com.muyie.apollo;
 
 import com.ctrip.framework.apollo.ConfigChangeListener;
-import com.ctrip.framework.apollo.model.ConfigChange;
 import com.ctrip.framework.apollo.model.ConfigChangeEvent;
 import com.ctrip.framework.apollo.spring.annotation.ApolloConfigChangeListener;
+import com.ctrip.framework.apollo.spring.config.PropertySourcesProcessor;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -14,11 +16,15 @@ import org.springframework.lang.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 /**
+ * 解决使用 `@ConfigurationProperties` 注解时，配置属性不更新的问题。
+ *
  * @author larry.qi
  * @date 2022-07-13
  */
 @Slf4j
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty({"apollo.bootstrap.enabled"})
+@ConditionalOnMissingBean({PropertySourcesProcessor.class})
 public class MuyieApolloAutoConfiguration implements ApplicationContextAware, ConfigChangeListener {
 
   private static ApplicationContext applicationContext = null;
@@ -35,10 +41,7 @@ public class MuyieApolloAutoConfiguration implements ApplicationContextAware, Co
   @Override
   @ApolloConfigChangeListener
   public void onChange(@NonNull ConfigChangeEvent event) {
-    event.changedKeys().forEach(key -> {
-      ConfigChange configChange = event.getChange(key);
-      log.info("Apollo ConfigChangeEvent - {}", configChange);
-    });
+    event.changedKeys().forEach(key -> log.info("Apollo ConfigChangeEvent - {}", event.getChange(key)));
     getApplicationContext().publishEvent(new EnvironmentChangeEvent(event.changedKeys()));
   }
 

e3b3ab54f2c7a676a14ab88f7f9f9051f7190a8e
qizhaohong <qizhaohong_sh@qq.com>
2022-08-24 16:25:04 +0800

    Initial commit

---
+++ muyie-starter-web/src/main/java/com/muyie/web/MuyieWebAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.muyie.web;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author larry.qi
+ * @since 1.2.6
+ */
+@Configuration(proxyBeanMethods = false)
+public class MuyieWebAutoConfiguration {
+
+}

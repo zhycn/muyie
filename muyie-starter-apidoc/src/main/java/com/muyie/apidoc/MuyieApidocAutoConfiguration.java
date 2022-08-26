bbe122a4fcdec1c750855873484b9e16e62aea4a
qizhaohong <qizhaohong_sh@qq.com>
2022-08-26 14:02:04 +0800

    Initial commit

---
+++ muyie-starter-apidoc/src/main/java/com/muyie/apidoc/MuyieApidocAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.muyie.apidoc;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author larry.qi
+ * @since 1.2.6
+ */
+@Configuration(proxyBeanMethods = false)
+public class MuyieApidocAutoConfiguration {
+
+}

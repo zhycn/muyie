7398fff5bc9e2f9171f9f192a802f2dd744aa9e7
qizhaohong <qizhaohong_sh@qq.com>
2022-08-29 10:10:12 +0800

    v1.2.7-SNAPSHOT commit

---
+++ muyie-starter-security/src/main/java/com/muyie/security/MuyieSecurityAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.muyie.security;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author larry.qi
+ * @since 1.2.7
+ */
+@Configuration(proxyBeanMethods = false)
+public class MuyieSecurityAutoConfiguration {
+
+}

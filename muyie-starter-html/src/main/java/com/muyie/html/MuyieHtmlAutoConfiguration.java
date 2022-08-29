c08fbc3f21f3c7adc34136956a3cffac3124cd29
qizhaohong <qizhaohong_sh@qq.com>
2022-08-29 13:44:43 +0800

    Initial commit

---
+++ muyie-starter-html/src/main/java/com/muyie/html/MuyieHtmlAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.muyie.html;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author larry.qi
+ * @since 1.2.7
+ */
+@Configuration(proxyBeanMethods = false)
+public class MuyieHtmlAutoConfiguration {
+
+
+}

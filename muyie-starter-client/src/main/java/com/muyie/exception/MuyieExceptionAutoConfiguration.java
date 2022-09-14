1c3bd26c942f2a533ad055f8932aa46202ab4e20
qizhaohong <qizhaohong_sh@qq.com>
2022-09-14 11:41:47 +0800

    v1.2.10-SNAPSHOT commit

---
+++ muyie-starter-client/src/main/java/com/muyie/exception/MuyieExceptionAutoConfiguration.java
@@ -0,0 +1,7 @@
+package com.muyie.exception;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class MuyieExceptionAutoConfiguration {
+}

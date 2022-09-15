b3da2aa0d56a9643c4d8fe75d99dfafe325187a0
qizhaohong <qizhaohong_sh@qq.com>
2022-09-15 17:49:46 +0800

    v1.2.11-SNAPSHOT commit

--- muyie-starter-orm/src/main/java/com/muyie/orm/dataobject/BaseAuditingDO.java
+++ muyie-starter-orm/src/main/java/com/muyie/orm/dataobject/BaseAuditingDO.java
@@ -1,18 +1,8 @@
 package com.muyie.orm.dataobject;
 
 import org.hibernate.validator.constraints.Length;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Version;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,36 +16,23 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseAuditingDO extends BasePersistentDO {
 
   private static final long serialVersionUID = 1L;
 
-  @Version
-  @Column(name = "revision")
   private Integer revision;
 
-  @CreatedBy
   @Length(max = 32)
-  @Column(name = "created_by", nullable = false, length = 32, updatable = false)
   private String createdBy;
 
-  @CreatedDate
-  @Column(name = "created_date", updatable = false)
   private Date createdDate;
 
-  @LastModifiedBy
   @Length(max = 32)
-  @Column(name = "last_modified_by", length = 32)
   private String lastModifiedBy;
 
-  @LastModifiedDate
-  @Column(name = "last_modified_date")
   private Date lastModifiedDate;
 
   @Length(max = 512)
-  @Column(name = "remark", length = 512)
   private String remark;
 
 }
--- muyie-starter-orm/src/main/java/com/muyie/orm/dataobject/BasePersistentDO.java
+++ muyie-starter-orm/src/main/java/com/muyie/orm/dataobject/BasePersistentDO.java
@@ -1,11 +1,6 @@
 package com.muyie.orm.dataobject;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.io.Serializable;
-
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
 
 /**
  * Base abstract class for persistent entities
@@ -13,8 +8,6 @@ import javax.persistence.MappedSuperclass;
  * @author larry.qi
  * @since 1.2.11
  */
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BasePersistentDO implements Serializable {
 
   private static final long serialVersionUID = 1L;

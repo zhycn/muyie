79b36c726db6448a66d234b7b5362f7c2e6f616d
qizhaohong <qizhaohong_sh@qq.com>
2022-11-03 14:26:26 +0800

    Initial thymeleaf

---
+++ muyie-starter-thymeleaf/src/main/java/com/muyie/thymeleaf/MuyieThymeleafAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.muyie.thymeleaf;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author larry.qi
+ * @since 1.3.1
+ */
+@Configuration(proxyBeanMethods = false)
+public class MuyieThymeleafAutoConfiguration {
+
+}

69cb6370cb8f303fbc4a7f01ebe3c0146edf50ab
qizhaohong <qizhaohong_sh@qq.com>
2023-07-20 17:25:17 +0800

    muyie-starter-security

--- muyie-starter-security/src/main/java/com/muyie/security/jwt/JwtSecurityConfiguration.java
+++ muyie-starter-security/src/main/java/com/muyie/security/jwt/JwtSecurityConfiguration.java
@@ -11,19 +11,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * JWT 安全配置
@@ -38,18 +40,12 @@ import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @Import({SecurityProblemSupport.class, JwtTokenProvider.class, CorsFilter.class})
+@RequiredArgsConstructor
 public class JwtSecurityConfiguration {
 
   private final JwtTokenProvider jwtTokenProvider;
   private final SecurityProblemSupport problemSupport;
   private final CorsFilter corsFilter;
-
-  public JwtSecurityConfiguration(JwtTokenProvider jwtTokenProvider, SecurityProblemSupport problemSupport,
-                                  CorsFilter corsFilter) {
-    this.jwtTokenProvider = jwtTokenProvider;
-    this.problemSupport = problemSupport;
-    this.corsFilter = corsFilter;
-  }
 
   @Bean
   @ConditionalOnMissingBean
@@ -67,37 +63,40 @@ public class JwtSecurityConfiguration {
   @Bean
   @ConditionalOnMissingBean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // 基于 token，不需要 csrf
     http.csrf().disable()
       .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
       .exceptionHandling()
       .authenticationEntryPoint(problemSupport)
       .accessDeniedHandler(problemSupport)
       .and().headers().frameOptions().disable()
+      // 基于 token，不需要 session
       .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and().authorizeRequests()
       .antMatchers("/api/**").authenticated()
       .antMatchers("/management/health").permitAll()
       .antMatchers("/management/info").permitAll()
       .antMatchers("/management/**")
-      .hasAuthority(AuthoritiesConstants.ROLE_ADMIN)
-      .and().apply(securityConfigurerAdapter());
+      .hasAuthority(AuthoritiesConstants.ROLE_ADMIN).and().apply(securityConfigurerAdapter());
     return http.build();
   }
 
   @Bean
   @ConditionalOnMissingBean
-  public UserDetailsService userDetailsService() {
-    UserDetails user = User.builder()
-      .username("user")
-      .password("password")
-      .roles("USER")
-      .build();
-    UserDetails admin = User.builder()
-      .username("admin")
-      .password("password")
-      .roles("ADMIN", "USER")
-      .build();
-    return new InMemoryUserDetailsManager(user, admin);
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * 获取 AuthenticationManager（认证管理器），登录时认证使用
+   *
+   * @param authenticationConfiguration 配置类
+   * @return 认证管理器
+   */
+  @Bean
+  @ConditionalOnMissingBean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 
   private JwtConfigurer securityConfigurerAdapter() {
